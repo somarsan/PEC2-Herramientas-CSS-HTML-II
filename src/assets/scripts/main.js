c49a5f3bf65391ffe88d0db2fdbb3072037f4279
somarsan <somarsan@uoc.edu>
2022-10-23 18:49:24 +0200

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,23 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+document.getElementById("hamburguesa").addEventListener("click", modificarMenu);
+let discriminador = true;
+window.addEventListener("resize", ocultarMenu);
+
+function modificarMenu() {
+  if (discriminador) {
+    document.querySelector("nav ul").style.left = "0px";
+    discriminador = false;
+  } else {
+    document.querySelector("nav ul").style.left = "-750px";
+    discriminador = true;
+  }
+}
+
+function ocultarMenu() {
+  if (window.innerWidth >= 800) {
+    document.querySelector("nav ul").style.left = "-750px";
+    discriminador = true;
+  }
+}
