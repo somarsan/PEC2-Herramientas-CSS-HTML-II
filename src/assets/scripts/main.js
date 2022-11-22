2ba813b01812e6ee018009466e0a2ee20556241a
somarsan <somarsan@uoc.edu>
2022-11-22 23:52:42 +0100

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,20 @@
  * see commented examples below
  */
 
+// import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
+
+
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
@@ -10,23 +24,3 @@
  * Write any other JavaScript below
  */
 
-document.getElementById("hamburguesa").addEventListener("click", modificarMenu);
-let discriminador = true;
-window.addEventListener("resize", ocultarMenu);
-
-function modificarMenu() {
-  if (discriminador) {
-    document.querySelector("nav ul").style.left = "0px";
-    discriminador = false;
-  } else {
-    document.querySelector("nav ul").style.left = "-750px";
-    discriminador = true;
-  }
-}
-
-function ocultarMenu() {
-  if (window.innerWidth >= 800) {
-    document.querySelector("nav ul").style.left = "-750px";
-    discriminador = true;
-  }
-}
